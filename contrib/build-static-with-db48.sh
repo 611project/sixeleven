1b5098f40f2feb136069938e037d32dc9a6dcf18
fflo <flo@degnet.de>
2019-12-28 09:23:16 +0000

    bugfix DB48 build error: atomic.h

--- contrib/build-static-with-db48.sh
+++ contrib/build-static-with-db48.sh
@@ -9,6 +9,8 @@ wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
 echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef  db-4.8.30.NC.tar.gz' | sha256sum -c
 # -> db-4.8.30.NC.tar.gz: OK
 tar -xzvf db-4.8.30.NC.tar.gz
+# bugfix DB48 build: atomic error
+patch db-4.8.30.NC/dbinc/atomic.h < db48-patch/atomic.patch
 
 # Build the library and install to our prefix
 cd db-4.8.30.NC/build_unix/
